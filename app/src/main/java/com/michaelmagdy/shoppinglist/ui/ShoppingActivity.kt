3e9ddc3f702f364a079ff75109878e5ffc30b150
MichaelTheBestProgrammerInTheWorld <michaelmagdy_93@yahoo.com>
2021-12-27 21:46:10 +0200

    Add dependency injection using kodein

--- app/src/main/java/com/michaelmagdy/shoppinglist/ui/ShoppingActivity.kt
+++ app/src/main/java/com/michaelmagdy/shoppinglist/ui/ShoppingActivity.kt
@@ -3,6 +3,7 @@ package com.michaelmagdy.shoppinglist.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.michaelmagdy.shoppinglist.R
@@ -10,18 +11,31 @@ import com.michaelmagdy.shoppinglist.ShoppingRepository
 import com.michaelmagdy.shoppinglist.db.ShoppingDatabase
 import com.michaelmagdy.shoppinglist.db.ShoppingItem
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
 
+//import org.kodein.di.generic.instance
+/*
+this import statement was missing which caused the following error
+PSI2IR: "org.jetbrains.kotlin.psi2ir.generators.ErrorExpressionException: null: KtCallExpression" with delegate who has name or parameter with the same name as a property
+ */
+import org.kodein.di.android.kodein
+
+
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
+    private lateinit var viewModel: ShoppingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
-        val viewModel = ViewModelProviders.of(this, factory)
+
+        viewModel = ViewModelProvider(this, factory)
             .get(ShoppingViewModel::class.java)
 
         val rvAdapter = ShoppingItemAdapter(listOf(), viewModel)
